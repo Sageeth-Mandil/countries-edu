c55ce1680869686a860d2554e80e85c261050d9e
Sageeth-Mandil <sageethmandil.me@gmail.com>
2024-03-17 15:53:41 +0530

    added images to the background

---
+++ src/components/body/Body.tsx
@@ -0,0 +1,35 @@
+// CountryDetails.tsx
+import React from 'react';
+import background from './../../assets/container-img.png' 
+import './Body.css'
+
+interface CountryDetailsProps {
+    country: any; // Replace 'any' with the type of your country details
+}
+
+const CountryDetails: React.FC<CountryDetailsProps> = ({ country }) => {
+    if (!country) {
+        return null;
+    }
+
+    const { name, capital, population, languages, flags } = country[0];
+
+    return (
+        <div className="container" >
+            <div className="card mt-4" id="img" style={{ backgroundImage: `url(${background})` }}>
+                
+                <img src={flags.svg} className="card-img-top" alt={`${name.common} Flag`} style={{ height: '300px', objectFit: 'contain' }} />
+                <div className="card-body">
+                    <h5 className="card-title text-center mb-4">{name.common}</h5>
+                    <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><strong>Capital:</strong> {capital}</li>
+                    <li className="list-group-item"><strong>Population:</strong> {population}</li>
+                    <li className="list-group-item"><strong>Languages:</strong> {Object.values(languages).join(', ')}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CountryDetails;
